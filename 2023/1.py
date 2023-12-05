beb69ebdfcd819570e469897cf024975cc3c1c52
Nathan <nslee333@gmail.com>
2023-12-04 21:00:06 -0800

    part 1 done, part 2 wip

---
+++ 2023/1.py
@@ -0,0 +1,79 @@
+import re
+
+# part 1
+def part_1():
+    inputs = []
+    count = 0
+
+
+    with open('input_1.txt', mode='r', encoding='utf-8') as f:
+        for x in f:
+            x = x.replace('\n', '')
+            inputs.append(x)
+
+    for item in inputs:
+        res = 0
+
+        matches = re.findall("[0-9]", item)
+        if matches:
+            if len(matches) >= 2:
+                res = int(str(matches[0]) + str(matches[-1]))
+            else:
+                res = int(str(matches[0]) + str(matches[0]))
+
+        count += res
+
+    return count
+
+
+def part_2():
+    inputs = []
+    count = 0
+
+
+    with open('input_1.txt', mode='r', encoding='utf-8') as f:
+        for x in f:
+            x = x.replace('\n', '')
+            inputs.append(x)
+
+    for item in inputs:
+        res = 0
+
+        print(item)
+        ex = "[0-9]|one|two|three|four|five|six|seven|eight|nine"
+
+        dict = {
+            'one':1, 
+            'two': 2, 
+            'three': 3, 
+            'four': 4, 
+            'five': 5, 
+            'six': 6, 
+            'seven': 7, 
+            'eight': 8, 
+            'nine': 9
+        }
+
+
+
+        matches = re.findall(ex, item)
+        if matches[0] in dict:
+            matches[0] = dict.get(matches[0])
+        if matches[-1] in dict:
+            matches[-1] = dict.get(matches[-1])
+            
+        if matches:
+            if len(matches) >= 2:
+                res = int(str(matches[0]) + str(matches[-1]))
+            else:
+                res = int(str(matches[0]) + str(matches[0]))
+            print(matches)
+        count += res
+        print(res)
+
+    return count
+    
+
+if __name__ == '__main__':
+    print(part_2())
+    # print(part_1())
