4527ea1041dff3eca0ac0c291ac2ef370f76f6aa
Nathan <nslee333@gmail.com>
2023-12-08 19:54:06 -0800

    atrocious wip

--- 2023/1.py
+++ 2023/1.py
@@ -28,18 +28,18 @@ def part_2():
     count = 0
 
 
-    with open('input_1.txt', mode='r', encoding='utf-8') as f:
+    with open('input_2.txt', mode='r', encoding='utf-8') as f:
         for x in f:
             x = x.replace('\n', '')
             inputs.append(x)
 
     for item in inputs:
-        res = 0
-
-        ex = "[0-9]|oneight|one|two|three|four|five|six|seven|eight|nine"
-
+        
+        start = 0
+        end = 0
+        
         dict = {
-            'one':1, 
+            'one': 1, 
             'two': 2, 
             'three': 3, 
             'four': 4, 
@@ -49,29 +49,69 @@ def part_2():
             'eight': 8, 
             'nine': 9
         }
-
-
-
-        matches = re.findall(ex, item)
         
-        if matches[0] == 'oneight':
-            matches[0] = 1
-        elif matches[0] in dict:
-            matches[0] = dict.get(matches[0])
+        arr = list(item)
+        matches = []
         
-        if matches[-1] == 'oneight':
-                matches[-1] = 8
-        elif matches[-1] in dict:
-            matches[-1] = dict.get(matches[-1])
+        while len(matches) < 1:
+            sub = []
+            for it in arr:
+                print(item)
+                print(sub)
+                sub.append(it)
+                
+                if it.isnumeric():
+                    matches.append(it)
+                    break
+                
+                if len(sub) >= 3 and len(sub) < 6:
+                    # print(str(sub))
+                    # print(''.join(sub) in dict, "DORK")
+                    if ''.join(sub) in dict:
+                        # print("what")
+                        matches.append(dict.get(''.join(sub)))
+                        break
+                    continue
+                # print(len(sub))
+                # print(len(sub) > 5)
+                if len(sub) > 5:
+                    # print("HIHIH")
+                    sub.pop(-2)
+                    sub.pop(-1)
+                    sub.pop(0)
+                    continue
+                
+        # print(item)
+        # print(matches)
             
-        if matches:
-            if len(matches) >= 2:
-                res = int(str(matches[0]) + str(matches[-1]))
-            else:
-                res = int(str(matches[0]) + str(matches[0]))
-        count += res
+        # while len(matches) < 2:
+            
+            
+                
+            
+            
+        
+        # ex = "[0-9]|oneight|one|two|three|four|five|six|seven|eight|nine"
 
-    return count
+        
+    
+    #     matches = re.findall(ex, item)
+        
+        
+    #     if matches[0] in dict:
+    #         matches[0] = dict.get(matches[0])
+        
+    #     if matches[-1] in dict:
+    #         matches[-1] = dict.get(matches[-1])
+            
+    #     if matches:
+    #         if len(matches) >= 2:
+    #             res = int(str(matches[0]) + str(matches[-1]))
+    #         else:
+    #             res = int(str(matches[0]) + str(matches[0]))
+    #     count += res
+
+    # return count
     
 
 if __name__ == '__main__':
